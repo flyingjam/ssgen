69a468839caff5aa53ef127d5151fd0f5b86d155
a person <tehflyingjam@gmail.com>
2016-03-16 23:09:30 -0400

    A

--- main.fs
+++ main.fs
@@ -6,36 +6,42 @@ let getLastDirectory (dir : string) =
     let length = dir.Length - index
     match index with
     | -1 -> dir
-    |_ -> dir.Substring(index, length)
+    |_ -> dir.Replace("\\", "/").Substring(index, length).Replace("/", "")
 
 let getFiles directory =
     Directory.GetFiles(directory) |> Seq.toList |> List.map (fun x -> Path.GetFileName(x))
 
+let fullPath directory =
+    Path.GetFullPath(directory)
+
 let recurFile directory =
-    let copyFiles d head =
-        let files = getFiles d
-        let dir = getLastDirectory(d)
-        printfn "head %s - dir %s" head dir
+    let copyFiles head =
+        let source = Path.Combine("input", head)
+        let output = Path.Combine("output", head)
+        printfn "head %s - dir %s" head source
+        let files = getFiles source
         printfn "%A" files
-        let source = Path.Combine(head, dir)
-        let output = Path.Combine(head, dir)
-        //files |> List.iter (fun x -> (File.Copy(Path.Combine(source, x), Path.Combine(output, x), true)))
+        files |> List.iter (fun x -> 
+            Directory.CreateDirectory(output);
+            (File.Copy(Path.Combine(source, x), Path.Combine(output, x), true)))
         files |> List.iter (fun x -> printfn "Source %s - Output %s" (Path.Combine(source, x)) (Path.Combine(output, x)))
-    let rec loop dir head =
-        copyFiles dir head
+
+    let rec loop head =
+        let dir = Path.Combine("input", head)
+        copyFiles head
         let sub = Directory.GetDirectories(dir) |> Seq.toList
         match sub with
         |[] -> ()
         |_ -> sub |> List.iter (fun x ->
                 //printfn "head %s - GetLast %s" head (getLastDirectory x);
-                loop x (Path.Combine(head, getLastDirectory dir)))
-    loop directory ""
+                loop (Path.Combine(head, getLastDirectory x)))
+    loop ""
     //copyFiles directory ""
     0
 [<EntryPoint>]
 let main args =
     printfn "hello world"
-    //let test = "input/sub1"
+    //let test = "input/sub1/"
     recurFile "input"
     //let p = getLastDirectory test
     //printfn "%s" p
